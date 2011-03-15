dfbbb5d865aa870fc524edc2fedf8403dede6568
Jake Luer <jake.luer@gmail.com>
2011-03-14 22:49:10 -0400

    revert to previous.

--- wppp.php
+++ wppp.php
@@ -265,7 +265,6 @@ class WPPP extends WP_Widget {
 			}
 			$query .= " WHERE p.id IN (" . implode( ',', $id_list ) . ")";
 			$query .= " AND p.post_status != 'draft' AND p.post_status != 'private' AND p.post_status != 'trash'";
-			$query .= " AND p.post_date >= DATE_SUB(NOW(), INTERVAL 30 DAY)";
 
 			// If I want to show only posts or only pages:
 			if ( $instance['show'] != 'both' ) {
